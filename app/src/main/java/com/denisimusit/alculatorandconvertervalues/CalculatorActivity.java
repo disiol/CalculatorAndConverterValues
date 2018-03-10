d0a3245a761427b99db4052d0fbc49e91b497258
denisimus <deoniisii@gmail.com>
2018-03-10 10:14:13 +0200

    make calculator logic

--- app/src/main/java/com/denisimusit/alculatorandconvertervalues/CalculatorActivity.java
+++ app/src/main/java/com/denisimusit/alculatorandconvertervalues/CalculatorActivity.java
@@ -2,9 +2,12 @@ package com.denisimusit.alculatorandconvertervalues;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CalculatorActivity extends AppCompatActivity {
     private TextView textViewInput;
@@ -29,7 +32,59 @@ public class CalculatorActivity extends AppCompatActivity {
     private Button button_interest;
     private Button button_point;
 
-    private String text;
+    private String histori;//записываетса история
+
+
+    /**
+     * Результат который заносится в масив для обработки
+     */
+    ArrayList<Float> result = new ArrayList<Float>();
+
+    /**
+     * Первое введенное число
+     */
+    float number1;
+
+    /**
+     * Второе введенное число
+     */
+    float number2;
+
+    int currentOperation = 0;
+    int nextOperation;
+
+    /**
+     * Прибавление
+     */
+    final static int ADD = 1;
+
+    /**
+     * Вычитание
+     */
+    final static int SUBTRACT = 2;
+
+    /**
+     * Умножение
+     */
+    final static int MULTIPLY = 3;
+
+    /**
+     * Деление
+     */
+    final static int DIVISION = 4;
+
+    /**
+     * Равно
+     */
+    final static int EQUALS = 5;
+
+
+    final static int CLEAR = 1;
+    final static int DONT_CLEAR = 0;
+    int clearCalcDisplay = 0;
+
+    private static final String TAG = "myLogs";
+    private static final int TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,99 +113,305 @@ public class CalculatorActivity extends AppCompatActivity {
         button_interest = (Button) findViewById(R.id.button_interest);
         button_point = (Button) findViewById(R.id.button_point);
 
+    }
 
-        //обробатываем нажатия кнопок
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.button_0:
-                        textViewInput.setText(R.string.button_0);
-                        break;
-                    case R.id.button_00:
-                        textViewInput.setText(R.string.button_00);
-                        break;
-                    case R.id.button_1:
-                        textViewInput.setText(R.string.button_1);
-                        break;
-                    case R.id.button_2:
-                        textViewInput.setText(R.string.button_2);
-                        break;
-                    case R.id.button_3:
-                        textViewInput.setText(R.string.button_3);
-                        break;
-                    case R.id.button_4:
-                        textViewInput.setText(R.string.button_4);
-                        break;
-                    case R.id.button_5:
-                        textViewInput.setText(R.string.button_5);
-                        break;
-                    case R.id.button_6:
-                        textViewInput.setText(R.string.button_6);
-                        break;
-                    case R.id.button_7:
-                        textViewInput.setText(R.string.button_7);
-                        break;
-                    case R.id.button_8:
-                        textViewInput.setText(R.string.button_8);
-                        break;
-                    case R.id.button_9:
-                        textViewInput.setText(R.string.button_9);
-                        break;
-                    case R.id.button_Clear:
-                        textViewInput.setText("");
-                        break;
-                    case R.id.button_minus:
-                        textViewInput.setText(R.string.button_minus);
-                        break;
-                    case R.id.button_plus:
-                        textViewInput.setText(R.string.button_plus);
-                        break;
-                    case R.id.button_multiply:
-                        textViewInput.setText(R.string.button_multiply);
-                        break;
-                    case R.id.button_divide:
-                        textViewInput.setText(R.string.button_divide);
-                        break;
-                    case R.id.button_point:
-                        textViewInput.setText(R.string.button_point);
-                        break;
-                    case R.id.button_interest:
-                        textViewInput.setText(R.string.button_interest);
-                        break;
-                    case R.id.button_del:
-                        //TODO
-                        break;
-                    case R.id.button_equals:
-                        //TODO
-                        break;
+    //обробатываем нажатия кнопок
+
+    public void onClick(View view) {
+        //TODO history
+        Log.d(TAG, "Оброботаем нажатие кнопки"); //выводит лог в Logcat
+//        CharSequence text = textViewInput.getText();
+
+        switch (view.getId()) {
+
+
+            case R.id.button_0:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("0");
+                break;
+
+            case R.id.button_00:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("00");
+
+                break;
+
+            case R.id.button_1:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().toString().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("1");
+                break;
+
+            case R.id.button_2:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("2");
+                break;
+
+            case R.id.button_3:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("3");
+                break;
+
+            case R.id.button_4:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("4");
+                break;
+
+            case R.id.button_5:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("5");
+                break;
+
+            case R.id.button_6:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("6");
+                break;
+
+            case R.id.button_7:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("7");
+                break;
+
+            case R.id.button_8:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.d(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("8");
+                break;
+
+            case R.id.button_9:
+                if (clearCalcDisplay == CLEAR) {
+                    textViewInput.setText("");
+
+                }
+                if (textViewInput.getText().equals("0")) {
+                    textViewInput.setText("");
+                    Log.e(TAG, "clearIfEqualsZero: ");
+                }
+                clearCalcDisplay = DONT_CLEAR;
+                textViewInput.append("9");
+                break;
+
+            case R.id.button_Clear:
+                textViewInput.setText("0");
+                result.removeAll(result);
+
+                break;
+
+            case R.id.button_plus:
+
+                if (textViewInput.length() != 0) {
+                    calcLogic(ADD);
+                }
+                Log.d(TAG, "Нажата кнопка +");
+                break;
+
+            case R.id.button_minus:
+                if (textViewInput.length() != 0) {
+                    calcLogic(SUBTRACT);
+
+                }
+                Log.d(TAG, "Нажата кнопка -");
+                break;
+
+            case R.id.button_divide:
+                if (textViewInput.length() != 0) {
+                    calcLogic(DIVISION);
 
                 }
 
+                Log.d(TAG, "Нажата кнопка /");
+                break;
+            case R.id.button_multiply:
+                calcLogic(MULTIPLY);
+                Log.d(TAG, "Нажата кнопка *");
+                break;
+
+            case R.id.button_equals:
+                Log.d(TAG, "Нажата кнопка =");
+
+                if (textViewInput.getText().length() != 0) {
+                    try {
+                        calcLogic(EQUALS);
+                    } catch (Exception e) {
+
+                        Log.e(TAG, "ошибка: " + e);
+                    }
+
+
+                }
+                break;
+
+
+            default:
+                break;
+
+        }
+    }
+
+
+    private void clearIfEqualsZero() {
+        if (textViewInput.getText().equals("0")) {
+            textViewInput.setText("");
+            Log.e(TAG, "clearIfEqualsZero: ");
+        }
+    }
+
+
+    /*Функция расчета введенных значений*/
+    private void calcLogic(int operator) {
+
+        try {
+            result.add(Float.parseFloat(textViewInput.getText().toString()));
+
+            if (operator != EQUALS) {
+                nextOperation = operator;
+            } else if (operator == EQUALS) {
+                nextOperation = 0;
             }
-        };
 
-        button_0.setOnClickListener(onClickListener);
-        button_00.setOnClickListener(onClickListener);
-        button_1.setOnClickListener(onClickListener);
-        button_2.setOnClickListener(onClickListener);
-        button_3.setOnClickListener(onClickListener);
-        button_4.setOnClickListener(onClickListener);
-        button_5.setOnClickListener(onClickListener);
-        button_6.setOnClickListener(onClickListener);
-        button_7.setOnClickListener(onClickListener);
-        button_8.setOnClickListener(onClickListener);
-        button_9.setOnClickListener(onClickListener);
-        button_minus.setOnClickListener(onClickListener);
-        button_plus.setOnClickListener(onClickListener);
-        button_divide.setOnClickListener(onClickListener);
-        button_multiply.setOnClickListener(onClickListener);
-        button_del.setOnClickListener(onClickListener);
-        button_interest.setOnClickListener(onClickListener);
-        button_point.setOnClickListener(onClickListener);
-        button_Clear.setOnClickListener(onClickListener);
-        button_equals.setOnClickListener(onClickListener);
+            switch (currentOperation) {
 
+        /*Прибавление*/
+                case ADD:
+                    number1 = result.get(0);
+                    number2 = result.get(1);
+
+                    result.removeAll(result);
+
+                    result.add(number1 + number2);
+
+                    textViewInput.setText(String.format("%.0f", result.get(0)));
+                    break;
+
+          /*Вычитание*/
+                case SUBTRACT:
+                    number1 = result.get(0);
+                    number2 = result.get(1);
+
+                    result.removeAll(result);
+
+                    result.add(number1 - number2);
+
+                    textViewInput.setText(String.format("%.0f", result.get(0)));
+                    break;
+
+          /*Умножение*/
+                case MULTIPLY:
+                    number1 = result.get(0);
+                    number2 = result.get(1);
+
+                    result.removeAll(result);
+
+                    result.add(number1 * number2);
+
+                    textViewInput.setText(String.format("%.0f", result.get(0)));
+                    break;
+           /*Деление*/
+                case DIVISION:
+                    number1 = result.get(0);
+                    number2 = result.get(1);
+
+                    result.removeAll(result);
+
+                    result.add(number1 / number2);
+
+                    textViewInput.setText(String.format("%.0f", result.get(0)));
+                    break;
+            }
+
+
+            clearCalcDisplay = CLEAR;
+            currentOperation = nextOperation;
+            if (operator == EQUALS) {
+                number1 = 0;
+                number2 = 0;
+                result.removeAll(result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void paused(int time) {
+
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "ошибка: " + e);
+        }
 
     }
+
+
 }
